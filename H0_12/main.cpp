0b779d1ef974ecd1570575da7db85c6b3bf20de8
Niko Kanamäki <nkanamak24@students.oamk.fi>
2025-10-27 22:41:58 +0200

    tunnin harjoitukset

---
+++ H0_12/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string firstName;
+    cout << "ANNA NIMESI:" << endl;
+    cin >> firstName;
+    cout<<"Hei! "<<firstName <<endl;
+    cin >> firstName;
+    return 0;
+}
