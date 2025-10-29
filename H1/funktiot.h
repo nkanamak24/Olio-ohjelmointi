2d79a2fe344ebc409796779a1b339a4f1f423084
Niko Kanamäki <nkanamak24@students.oamk.fi>
2025-10-29 22:08:39 +0200

    H1

---
+++ H1/funktiot.h
@@ -0,0 +1,11 @@
+#ifndef FUNKTIOT_H
+#define FUNKTIOT_H
+#include <iostream>
+using namespace std;
+
+void calcSum(int a, int b);
+void calcDiv(int a, int b);
+int retSum(int a, int b);
+float retDiv(int a, int b);
+
+#endif
